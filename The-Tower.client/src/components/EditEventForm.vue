d0b0e1ac0342d0e3d1738a55646d2683f1909b9e
Kevin Kelley <ksquaredcoding@gmail.com>
2022-10-07 15:43:23 -0600

    access

--- The-Tower.client/src/components/EditEventForm.vue
+++ The-Tower.client/src/components/EditEventForm.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="offcanvas offcanvas-end" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="eventEdit"
-    aria-labelledby="eventsCanvasLabel">
+    aria-labelledby="editEventsCanvasLabel">
     <div class="offcanvas-header">
-      <h5 class="offcanvas-title" id="postsCanvasLabel">Edit Event</h5>
+      <h5 class="offcanvas-title" id="editEventsCanvasLabel">Edit Event</h5>
       <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
     <div class="offcanvas-body">
